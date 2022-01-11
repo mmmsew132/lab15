9792e03e412998337c9dd11d7b5ea6ebfa5cecd8
mmmsew <yayachaidan@gmail.com>
2022-01-11 22:38:23 +0700

    15_1 task failed successfully

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,19 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for (int i = 0; i < N-1; i++) {
+		int a = d[i+1];
+		int j = i+1;
+		while (a>d[j-1]) {
+			d[j] = d[j-1];
+			j--;
+			if (j==0) break;
+		}
+		d[j] = a;
+		cout << "Pass " << i+1 << ":";
+		for (int k = 0; k < N; k++) cout << d[k] << " ";
+		cout << "\n";
+	}
 }
 
 int main(){
